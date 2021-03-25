da4f8efa243aee7f475f3ce4354842ba74f6d8a1
Igor Mosunov <imosunov@nsg.ru>
2021-03-25 13:01:54 +0300

    get SSL certs from env var

--- config/local.exs
+++ config/local.exs
@@ -1,5 +1,13 @@
 import Config
 
 config :nsg_lora, NsgLoraWeb.Endpoint,
-  url: [host: "localhost", port: 4000],
   http: [port: 4000]
+  config :nsg_lora, NsgLoraWeb.Endpoint,
+    https: [
+      port: 4443,
+      cipher_suite: :strong,
+      keyfile: System.get_env("NSG_LORA_SSL_KEY_PATH") || "priv/cert/selfsigned_key.pem",
+      certfile: System.get_env("NSG_LORA_SSL_CERT_PATH") || "priv/cert/selfsigned.pem",
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    url: [host: "localhost", port: 4443]
--- config/target.exs
+++ config/target.exs
@@ -1,12 +1,11 @@
 import Config
 
-# TODO add reading params from env
 config :nsg_lora, NsgLoraWeb.Endpoint,
   https: [
     port: 4443,
     cipher_suite: :strong,
-    keyfile: "/etc/stunnel/https_auto.key",
-    certfile: "/etc/stunnel/https_auto.cert",
+    keyfile: System.get_env("NSG_LORA_SSL_KEY_PATH") || "priv/cert/selfsigned_key.pem",
+    certfile: System.get_env("NSG_LORA_SSL_CERT_PATH") || "priv/cert/selfsigned.pem",
     transport_options: [socket_opts: [:inet6]]
   ],
   url: [host: "localhost", port: 4443]
@@ -14,5 +13,4 @@ config :nsg_lora, NsgLoraWeb.Endpoint,
 config :mnesia,
   dir: '/usr/lib/lora/Mnesia'
 
-config :nsg_lora, :lora,
-  gpio_reset_pin: 27
+config :nsg_lora, :lora, gpio_reset_pin: 27
