9b3fbdf1c35f94d9456019651df5bef846f39d16
Igor Mosunov <imosunov@nsg.ru>
2021-04-01 13:38:15 +0300

    DivIcon

--- assets/js/live_map.js
+++ assets/js/live_map.js
@@ -11,12 +11,35 @@ if (livemap) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
 
+  let sensorIcon = function(dbm, distance) {
+    return L.divIcon({
+      html: `<div class="flex flex-col justify-center h-8 w-8 rounded-full bg-red-500 bg-opacity-50 border border-gray-900  text-center text-gray-900 tracking-tighter leading-none"><div>${dbm}</div><div>${distance}</div></div>`,
+      className: 'nsg-sensor-icon'
+    });
+  }
+
+  let marker = {
+    date: "2021-04-01T13:21:30",
+    freq: 867.3,
+    lat: 55.7775,
+    lon: 37.7382,
+    lsnr: 11,
+    rssi: -43
+  }
+
+  L.marker([55.7782, 37.7371], {
+      icon: sensorIcon(marker.rssi, marker.distance || ""),
+    })
+    .bindPopup(`${marker.date}<br>${marker.lat} ${marker.lon}<br>freq: ${marker.freq}<br>rssi: ${marker.rssi}<br>lsnr: ${marker.lsnr}`).openPopup()
+    .addTo(mymap);
+
+
   global.addMarkerToLiveMap = function(marker) {
-    var circle = L.circle([marker.lat, marker.lon], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 5
-    }).addTo(mymap);
+    console.log(marker)
+    L.marker([marker.lat, marker.lon], {
+        icon: sensorIcon(marker.rssi, marker.distance || ""),
+      })
+      .bindPopup(`${marker.date}<br>${marker.lat} ${marker.lon}<br>freq: ${marker.freq}<br>rssi: ${marker.rssi}<br>lsnr: ${marker.lsnr}`).openPopup()
+      .addTo(mymap);
   }
 }
